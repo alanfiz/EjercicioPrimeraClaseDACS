cef94469d110c00e4608227e5ce5152dbad11313
alanfiz <alanfiz92@gmail.com>
2017-03-15 20:02:57 -0300

    Nuevas clases PoliticasSimple/Compuesta. Atributos interfaz Politica.

--- src/Politica.java
+++ src/Politica.java
@@ -1,7 +1,15 @@
+import java.util.Map;
+
 /**
  * Created by alan on 13-Mar-17.
  */
 public interface Politica {
+
+    //atributos de politica//
+    private String nombrePolitica ;
+    private Map<String, String> atributos ;
+
+
 
 
 }
